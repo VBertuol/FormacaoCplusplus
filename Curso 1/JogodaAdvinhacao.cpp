30ed08aaa88ee8d2a8b06d19e298d137cf76cff2
Vinicius Bertuol <viniciusbertuol@alunos.utfpr.edu.br>
2022-03-10 12:34:07 -0300

    criação do arquivo inicial

---
+++ Curso 1/JogodaAdvinhacao.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+
+int main () {
+    int numeroSecreto = 42;
+    std::cout << "************************************\n* Bem-vindo ao jogo da adivinhacao!*\n************************************";
+
+
+    return 0;
+}
